6c4b6cc17d47c45a27720ef344a583acf2597e8e
Ray Adams <63924749+Ray-Adams@users.noreply.github.com>
2020-05-31 14:07:47 -0700

    Update bookmarklet.beautified.js

--- bookmarklet.beautified.js
+++ bookmarklet.beautified.js
@@ -2,50 +2,105 @@ javascript:(function(){
   
   var elem='
     <div id="bookmarklet" style="
-      position:fixed;
-      background:#FFF; 
+      position: fixed;
+      background: #FFF; 
       border: 1px #000 solid; 
-      right:50px; 
-      top:50px; 
-      z-index:1000; 
-      width:250px;
+      right: 50px; 
+      top: 50px; 
+      z-index: 1000; 
+      width: 250px;
     ">
+  
     <form>
+      
       <label for="url">
         URL:
       </label>
+  
       <select name="url-input" id="url-input">
-        <option value="https://www.nitrotype.com/api/settings">
+        
+	      <optgroup label="Authentication">
+	        <option value="https://www.nitrotype.com/api/auth/facebook">
+            */auth/facebook
+          </option>
+	      </optgroup>
+  
+	      <optgroup label="Classes">
+	        <option value="https://www.nitrotype.com/api/classes">
+            */classes
+          </option>
+	      </optgroup>
+	    
+        <optgroup label="Friends">
+	        <option value="https://www.nitrotype.com/api/friends">
+            */friends
+          </option>
+	      </optgroup>
+  
+	      <optgroup label="News">
+	        <option value="https://www.nitrotype.com/api/news">
+            */news
+          </option>
+	      </optgroup>
+  
+	      <optgroup label="Purchase">
+	        <option value="https://www.nitrotype.com/api/purchase/refreshsession">
+            */purchase/refreshsession
+          </option>
+	      </optgroup>
+	
+       <optgroup label="Referrals">
+	        <option value="https://www.nitrotype.com/api/referrals">
+            */referrals
+          </option>
+	     </optgroup>       
+  
+	    <optgroup label="Settings">
+	      <option value="https://www.nitrotype.com/api/settings">
           */settings
         </option>
+	    </optgroup>
+	
+      <optgroup label="Stats">
         <option value="https://www.nitrotype.com/api/stats/summary">
           */stats/summary
         </option>
-        <option value="https://www.nitrotype.com/api/auth/facebook">
-          */auth/facebook
+	      <option value="https://www.nitrotype.com/api/stats/graphs/bymonth">
+          */stats/graphs/bymonth
         </option>
-        <option value="https://www.nitrotype.com/api/friends">
-          */friends
-        </option>
-        <option value="https://www.nitrotype.com/api/referrals">
-          */referrals
-        </option>
-        <option value="https://www.nitrotype.com/api/stats/graphs/lastdays">
+	      <option value="https://www.nitrotype.com/api/stats/graphs/lastdays">
           */stats/graphs/lastdays
         </option>
         <option value="https://www.nitrotype.com/api/stats/graphs/racelog">
           */stats/graphs/racelog
         </option>
-        <option value="https://www.nitrotype.com/api/teams/applications">
+        <option value="https://www.nitrotype.com/api/stats/data/bymonth">
+          */stats/data/bymonth
+        </option>
+	      <option value="https://www.nitrotype.com/api/stats/data/lastdays">
+          */stats/data/lastdays
+        </option>
+	      <option value="https://www.nitrotype.com/api/stats/data/racelog">
+          */stats/data/racelog
+        </option>
+	    </optgroup>
+	   
+      <optgroup label="Teams">
+	      <option value="https://www.nitrotype.com/api/teams/applications">
           */teams/applications
         </option>
+	    </optgroup>
+  
       </select>
+  
       <br><br>
+            
       <button id="add-tag">
         <strong>
           Click to Submit
         </strong>
       </button>
+  
     </form>
     </div>';
     
