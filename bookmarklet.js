22c48a1926e22804be5acd21a4b252000c194726
Ray Adams <63924749+Ray-Adams@users.noreply.github.com>
2020-07-02 16:02:52 -0700

    Rename to update jsDelivr CDN

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:(function(){var e=document.createElement('script');e.type='text/javascript';e.src='https://cdn.jsdelivr.net/gh/Ray-Adams/NitroType-Get-Requests/GET_Requests.js';document.getElementsByTagName('body')[0].appendChild(e);})();
+javascript:(function(){var e=document.createElement('script');e.type='text/javascript';e.src='https://cdn.jsdelivr.net/gh/Ray-Adams/NitroType-Get-Requests/get_requests.js';document.getElementsByTagName('body')[0].appendChild(e);})();
