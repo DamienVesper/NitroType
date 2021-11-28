76d5187a50a7acb1e86b5eb287b0d915964e38fd
Ray Adams <ask.rayadams@protonmail.com>
2021-11-28 07:17:39 -0800

    Delete bookmarklet.js

--- bookmarklet.js
+++
@@ -1,1 +0,0 @@
-javascript:(function(){var e=document.createElement('script');e.type='text/javascript';e.src='https://cdn.jsdelivr.net/gh/Ray-Adams/NitroType-Get-Requests/get_requests.js';document.getElementsByTagName('body')[0].appendChild(e);})();
