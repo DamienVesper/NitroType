145fd27638b591887f02f1967e580820d546c54e
Ray Adams <pm.rayadams@protonmail.com>
2022-03-11 16:28:10 -0800

    fix: friends race functionality

--- scripts/auto_reload.user.js
+++ scripts/auto_reload.user.js
@@ -1,20 +1,18 @@
 // ==UserScript==
 // @name         NitroType Auto Reload
 // @namespace    https://github.com/Ray-Adams
-// @version      2.0.0
+// @version      2.0.2
 // @description  Automatically reloads the page at the end of each race.
 // @author       Ray Adams
 // @match        *://*.nitrotype.com/race
+// @match        *://*.nitrotype.com/race/
 // @run-at       document-end
 // @grant        none
+// @license      MIT
+// @homepageURL  https://github.com/Ray-Adams/NitroType-Archive
 // ==/UserScript==
 
-/***************************************
-         
-    Ray-Adams/NitroType-Auto-Reload
-  Jun 21, 2020 - Updated Feb 22, 2021
-      
-****************************************/
+console.log("'NitroType Auto Reload' is no longer maintained, see https://github.com/Ray-Adams/NitroType-Archive for more info.")
 
 const options = {
     timerMs: 1000,
